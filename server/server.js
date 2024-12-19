e1ed01c53459984f45d272c9a95eb08c448570b4
Katsumac <jng193@my.bcit.ca>
2024-12-18 19:18:32 -0800

    initialize project

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+    res.json({message: "Home Page"});
+});
+
+app.get("/login", (req, res) => {
+    res.json({message: "Login Page"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+});
