3a5c4f960b25365ce8451267400ea4f2c29e2692
Katsumac <jng193@my.bcit.ca>
2025-01-02 21:25:28 -0800

    Populated Artifacts page

--- client/src/Artifacts.jsx
+++ client/src/Artifacts.jsx
@@ -1,9 +1,38 @@
+import { useState, useEffect } from 'react';
+
+import Grid from '@mui/material/Grid2'
+import Typography from '@mui/material/Typography';
 import ArtifactCard from "./components/ArtifactCard"
 
 export default function Artifacts() {
-    return(
+
+    const [artifactList, setArtifactList] = useState([]);
+
+    useEffect(() => {
+        fetch("https://genshin.jmp.blue/artifacts")
+        .then(response => response.json())
+        .then(data => setArtifactList(data))
+        .catch(e => `Error: ${e}`);
+
+        document.title = "Artifacts | Genshindex";
+    }, [])
+
+    return (
         <>
-        <ArtifactCard artifactName="prayers-for-destiny" />
-        </>
+        <Typography variant="h3" component="h2" sx={{mb: 6}}>Artifacts</Typography>
+        <Grid
+            container
+            columnSpacing={{xs: 1, sm: 1, md: 1}}
+            rowSpacing={{xs: 2, md: 5}}
+            columns={{xs: 2, sm: 6, md: 12}}
+            justifyContent={"space-evenly"}
+            sx={{mb: 6}}>
+                {artifactList.map((artifact, i) => {
+                    return <Grid key={i} size={{xs: 1, sm: 2, md: 3}} display="flex" justifyContent={'center'}>
+                                <ArtifactCard artifactName={artifact} key={artifact + "Card"}/>
+                            </Grid>
+                })}
+        </Grid>
+    </>
     )
 }
