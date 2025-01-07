a86157dcd19ce55f303f2e1ad99845a635c605a7
Katsumac <jng193@my.bcit.ca>
2025-01-06 22:05:40 -0800

    Created NotFound page

---
+++ client/src/NotFound.jsx
@@ -0,0 +1,12 @@
+import Typography from '@mui/material/Typography';
+import "./style/NotFound.css"
+
+export default function NotFound() {
+    return (
+        <div>
+            <Typography variant="h3" component="h2" sx={{mb: 4}}>Uh oh! Paimon couldn't find the page!</Typography>
+            <img src="./src/img/surprisedPaimon.png" id="surprisedPaimon"/>
+            <Typography variant="body1" component="h2" sx={{mt: 4}}>How about we explore the page ahead of us later?</Typography>
+        </div>
+    )
+}
