c76dc55cf1da5b7feac4c039b065212f75b56ce8
Katsumac <jng193@my.bcit.ca>
2025-01-07 20:19:22 -0800

    Populated 4 cards from each category

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -7,30 +7,30 @@ import ArtifactCard from "./components/ArtifactCard";
 import FoodCard from "./components/FoodCard";
 
 export default function Home() {
-    const [characterData, setCharacterData] = useState("");
-    const [weaponData, setWeaponData] = useState("");
-    const [artifactData, setArtifactData] = useState("");
-    const [foodData, setFoodData] = useState("");
+    const [characterList, setCharacterList] = useState([]);
+    const [weaponList, setWeaponList] = useState([]);
+    const [artifactList, setArtifactList] = useState([]);
+    const [foodList, setFoodList] = useState([]);
 
     useEffect(() => {
         fetch("https://genshin.jmp.blue/characters")
         .then(response => response.json())
-        .then(data => setCharacterData(data))
+        .then(data => setCharacterList(data.slice(0, 4)))
         .catch(e => console.log(`Error: ${e}`));
 
         fetch("https://genshin.jmp.blue/weapons")
         .then(response => response.json())
-        .then(data => setWeaponData(data))
+        .then(data => setWeaponList(data.slice(0, 4)))
         .catch(e => console.log(`Error: ${e}`));
 
         fetch("https://genshin.jmp.blue/artifacts")
         .then(response => response.json())
-        .then(data => setArtifactData(data))
+        .then(data => setArtifactList(data.slice(0, 4)))
         .catch(e => console.log(`Error: ${e}`));
 
         fetch("https://genshin.jmp.blue/consumables/food")
         .then(response => response.json())
-        .then(data => setFoodData(data))
+        .then(data => setFoodList(Object.entries(data).slice(0, 4)))
         .catch(e => console.log(`Error: ${e}`));
 
         document.title = "GenshinDex";
@@ -39,10 +39,22 @@ export default function Home() {
     return (
         <div>
             <Typography variant="h3" component="h2" sx={{mb: 6}}>Welcome to GenshinDex!</Typography>
-            <CharacterCard characterName="albedo" />
+            {characterList.map(character => {
+                return <CharacterCard characterName={character} />
+            })}
+            {weaponList.map(weapon => {
+                return <WeaponCard weaponName={weapon} />
+            })}
+            {artifactList.map(artifact => {
+                return <ArtifactCard artifactName={artifact} />
+            })}
+            {foodList.map(food => {
+                return <FoodCard foodName={food[0]} />
+            })}
+            {/* <CharacterCard characterName="albedo" />
             <WeaponCard weaponName="harbinger-of-dawn" />
             <ArtifactCard artifactName="archaic-petra" />
-            <FoodCard foodName="adeptus-temptation" />
+            <FoodCard foodName="adeptus-temptation" /> */}
         </div>
     )
 }
