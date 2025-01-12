132e67be99d812efbe126e4a70f646a022b55247
Katsumac <jng193@my.bcit.ca>
2025-01-12 15:22:51 -0800

    Created search bar and button

--- client/src/Characters.jsx
+++ client/src/Characters.jsx
@@ -2,11 +2,16 @@ import { useState, useEffect } from 'react';
 
 import Grid from '@mui/material/Grid2'
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import CharacterCard from "./components/CharacterCard";
+import "./style/SearchBar.css";
 
 export default function Characters() {
 
     const [characterList, setCharacterList] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
         fetch("https://genshin.jmp.blue/characters")
@@ -19,15 +24,20 @@ export default function Characters() {
 
     return (
         <>
-            
             <Typography variant="h3" component="h2" sx={{mb: 6}}>Characters</Typography>
+            <div className="searchBar">
+                <Stack direction="row">
+                    <TextField fullWidth id="outlined-basic" label="Search" variant="outlined" size="medium" sx={{backgroundColor: "white"}} />
+                    <Button variant="contained" sx={{backgroundColor: "#ffc000"}}> Search </Button>
+                </Stack>
+            </div>
             <Grid
                 container
                 columnSpacing={{xs: 1, sm: 1, md: 1}}
                 rowSpacing={{xs: 2, md: 5}}
                 columns={{xs: 2, sm: 6, md: 12}}
                 justifyContent={"space-evenly"}
-                sx={{mb: 6}}>
+                sx={{my: 6}}>
                     {characterList.map((character, i) => {
                         return <Grid key={i} size={{xs: 1, sm: 2, md: 3}} display="flex" justifyContent={'center'}>
                                     <CharacterCard characterName={character} key={character + "Card"}/>
