a8f91379b87ec32127d45483b333e52c8ad22828
Katsumac <jng193@my.bcit.ca>
2025-01-13 23:49:08 -0800

    Added search functionality to all data pages

--- client/src/Characters.jsx
+++ client/src/Characters.jsx
@@ -9,6 +9,15 @@ import "./style/Paragraph.css"
 export default function Characters() {
 
     const [characterList, setCharacterList] = useState([]);
+        
+    useEffect(() => {
+        fetch("https://genshin.jmp.blue/characters")
+        .then(response => response.json())
+        .then(data => setCharacterList(data))
+        .catch(e => `Error: ${e}`);
+
+        document.title = "Characters | Genshindex";
+    }, [])
 
     const searchCharacters = (query) => {
         fetch("https://genshin.jmp.blue/characters")
@@ -18,15 +27,6 @@ export default function Characters() {
         })
         .catch(e => `Error: ${e}`);
     }
-
-    useEffect(() => {
-        fetch("https://genshin.jmp.blue/characters")
-        .then(response => response.json())
-        .then(data => setCharacterList(data))
-        .catch(e => `Error: ${e}`);
-
-        document.title = "Characters | Genshindex";
-    }, [])
 
     return (
         <>
@@ -38,7 +38,8 @@ export default function Characters() {
                 <Typography variant="body2" component="h2" sx={{mt: 3, mb: 6, maxWidth: 900}}>
                     For the Kamisato siblings, please search by their first name.
                     If a character's name has two or more words, you may need use a dash in place of a space.
-                    For example: "traveler-anemo", or "arataki-itto".</Typography>
+                    For example: "traveler-anemo", or "arataki-itto".
+                    Of course, you may search with part of their names instead.</Typography>
             </div>
             <Grid
                 container
@@ -51,8 +52,8 @@ export default function Characters() {
                         return <Grid key={i} size={{xs: 1, sm: 2, md: 3}} display="flex" justifyContent={'center'}>
                                     <CharacterCard characterName={character} key={character + "Card"}/>
                                 </Grid>
-                    }) : <Typography variant="body2" component="h2" sx={{mt: 3, mb: 6, maxWidth: 900}}> No characters found. </Typography>
-}
+                        }) : <Typography variant="body2" component="h2" sx={{mt: 3, mb: 6, maxWidth: 900}}> No characters found. </Typography>
+                    }
             </Grid>
         </>
     )
--- client/src/Weapons.jsx
+++ client/src/Weapons.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid2'
 import Typography from '@mui/material/Typography';
 import WeaponCard from "./components/WeaponCard"
+import SearchBar from './components/SearchBar';
+import "./style/Paragraph.css"
 
 export default function Weapons() {
 
@@ -17,9 +19,28 @@ export default function Weapons() {
         document.title = "Weapons | GenshinDex";
     }, [])
 
+    const searchWeapons = (query) => {
+        fetch("https://genshin.jmp.blue/weapons")
+        .then(response => response.json())
+        .then(data => {
+            setWeaponList(data.filter(name => name.includes(query)));
+        })
+        .catch(e => `Error: ${e}`);
+    }
+
     return (
         <>
             <Typography variant="h3" component="h2" sx={{mb: 6}}>Weapons</Typography>
+            <div className="searchBar">
+                <SearchBar runQuery={searchWeapons} />
+            </div>
+            <div className="paragraph">
+                <Typography variant="body2" component="h2" sx={{mt: 3, mb: 6, maxWidth: 900}}>
+                    For "Amos' Bow", please search by "amos-bow".
+                    If a weapon's name has two or more words, or has an apostrophe, you may need use a dash in place of a space/apostrophe.
+                    For example: "a-thousand-floating-dreams", or "beginner-s-protector".
+                    Of course, you may search with part of their names instead.</Typography>
+            </div>            
             <Grid
                 container
                 columnSpacing={{xs: 1, sm: 1, md: 1}}
@@ -27,11 +48,12 @@ export default function Weapons() {
                 columns={{xs: 2, sm: 6, md: 12}}
                 justifyContent={"space-evenly"}
                 sx={{mb: 6}}>
-                    {weaponList.map((weapon, i) => {
+                    {weaponList.length !== 0 ? weaponList.map((weapon, i) => {
                         return <Grid key={i} size={{xs: 1, sm: 2, md: 3}} display="flex" justifyContent={'center'}>
                                     <WeaponCard weaponName={weapon} key={weapon + "Card"}/>
                                 </Grid>
-                    })}
+                        }) : <Typography variant="body2" component="h2" sx={{mt: 3, mb: 6, maxWidth: 900}}> No weapons found. </Typography>
+                    }
             </Grid>
         </>
     )
