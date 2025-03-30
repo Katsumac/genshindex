20399e467a01b48f35b247c5fccf2a9900961a53
Katsumac <jng193@my.bcit.ca>
2025-03-30 15:37:26 -0700

    Initialized local db

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const corsOptions = {
-    origin: ["http://localhost:5173"]
-};
+const mysql = require("mysql2");
 
+// const corsOptions = {
+//     origin: ["http://localhost:5173"]
+// };
 const PORT = process.env.PORT || 8080;
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({message: "Home Page"});
 });
+
+app.get("/users", (req, res) => {
+    const db = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: '',
+        port: 3307,
+        database: "genshindex"
+    });
+    
+    db.connect();
+    db.query("SELECT * FROM test", (error, data) => {
+        if (error) {
+            console.log(error);
+        }
+        res.json(data);
+        db.end();
+    });
+})
 
 app.get("/login", (req, res) => {
     res.json({message: "Login Page"});
